6f656e26064a192c6ad38d8426cbe1136d480acc
Taro Nagasawa <taro.nagasawa@gmail.com>
2015-07-05 16:59:46 +0900

    ぼくなりの修正

--- src/main/java/highandlow/HighAndLow.java
+++ src/main/java/highandlow/HighAndLow.java
@@ -3,135 +3,141 @@ package highandlow;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 /**
  * High and Low ゲームクラス
  */
-class HighAndLow{
+class HighAndLow {
     public static void main(String[] args) {
-        // スタート文言の表示
-        System.out.println("High and Low ゲームをはじめます。");
-        // 基準値の生成
-        // finalつけたい
-        int first = makeRandomNumber();
-        // ユーザの入力
-        System.out.println(first + "! 次の値は High? Low?");
-        int userAnswer = inputUserAnswer();
-        // 勝負値の生成
-        int second = makeRandomNumber();
-        System.out.println(second + "がでました！");
-        // 結果判定
-        compareNumber(first, second, userAnswer);
-        System.out.println("ゲームを終わります。");
+        final GameController controller = newGameController();
+        controller.start();
+        controller.inputUserAnswer();
+        controller.showResult();
+        controller.finish();
     }
 
-    /**
-     * ランダム数の生成を行います。
-     * 最大値・最小値の指定も今回はこのメソッド内で設定しています。
-     * 最小値：1 最大値：10
-     * @return int 乱数
-     */
-    // privateでいいような
-    protected static int makeRandomNumber(){
-        return (int)(Math.random() * 10 + 1);
+    private static GameController newGameController() {
+        final Game game = new Game(makeRandomNumber(), makeRandomNumber());
+        return new GameController(game);
     }
 
-    /**
-     * ユーザの答えの入力を制御します。
-     * 1: High
-     * 2: Low
-     * 入力チェックも合わせて実施します。
-     * @return
-     */
-    protected static int inputUserAnswer(){
-        String choices = "1: High   2: Low";
-        System.out.println(choices);
+    static int makeRandomNumber() {
+        return (int) (Math.random() * 10 + 1);
+    }
 
-        String userAnswer = "";
-        // try-with-resourcesいいね( ๑°ω°๑)و ｸﾞｯ!
-        try(InputStreamReader is = new InputStreamReader(System.in);
-            BufferedReader br = new BufferedReader(is)) {
+    static class GameController {
 
-            // ユーザの入力を待ちます
-            userAnswer = br.readLine();
+        private static final Pattern inputPattern = Pattern.compile("^[12]$");
 
-            // 正しい値が入力されるまで入力させ続けます。
-            while(!isChoice(userAnswer)) {
-                System.out.println("入力された値が不正です。正しい値を入力してください。");
-                System.out.println(choices);
-                userAnswer = br.readLine();
+        private final Game game;
+
+        private Optional<Answer> answer = Optional.empty();
+
+        public GameController(final Game game) {
+            this.game = game;
+        }
+
+        /**
+         * 開始
+         */
+        public void start() {
+            System.out.println("High and Low ゲームをはじめます。");
+        }
+
+        /**
+         * 入力
+         */
+        public void inputUserAnswer() {
+            System.out.println(game.getFirstNumber() + "! 次の値は High? Low?");
+            System.out.println("1: High   2: Low");
+            this.answer = getInputUserAnswer();
+        }
+
+        private static Optional<Answer> getInputUserAnswer() {
+            try (final BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                while (true) {
+                    final String input = br.readLine();
+                    if (inputPattern.matcher(input).matches()) {
+                        final Answer answer = input.equals("1") ? Answer.HIGH : Answer.LOW;
+                        return Optional.of(answer);
+                    }
+
+                    System.out.println("入力された値が不正です。正しい値を入力してください。");
+                }
+            } catch (IOException e) {
+                return Optional.empty();
             }
-
-        } catch (IOException ioe) {
-            System.out.println("入力値に誤りがあります。ゲームを始めからやりなおしてください。 入力値：" + userAnswer);
         }
 
-        return Integer.parseInt(userAnswer);
+        /**
+         * 結果表示
+         */
+        public void showResult() {
+            final Answer answer = this.answer.orElseThrow(IllegalStateException::new);
+            System.out.println(game.getSecondNumber() + "がでました！");
+            final Result result = game.go(answer);
+            System.out.println(getResultMessage(result));
+        }
+
+        private String getResultMessage(final Result result) {
+            switch (result) {
+                case WIN:
+                    return "あたり！ﾜ━ヽ(*´Д｀*)ﾉ━ｨ!!!";
+                case LOSE:
+                    return "はずれ！(´・ω・｀)";
+                case DRAW:
+                    return "引き分け！";
+            }
+            throw new IllegalArgumentException();
+        }
+
+        /**
+         * 終了
+         */
+        public void finish() {
+            System.out.println("ゲームを終わります。");
+        }
+
+        public Optional<Answer> getAnswer() {
+            return answer;
+        }
     }
 
-    /**
-     * 選択肢の入力チェックを行います。
-     * - 数値型であること
-     * - 1か2の値であること（1:High 2:Low)
-     * @param targetChoice 入力チェック対象
-     * @return
-     */
-    protected static boolean isChoice(String targetChoice) {
-        //必須チェック
-        if (targetChoice.isEmpty()) {
-            return false;
-        }
-
-        //数値型チェック
-        // pattern、キャッシュしたほうがいいかも
-        // キャッシュしないならString#matches使ったほうが読みやすい
-        Pattern pattern = Pattern.compile("^[0-9]*$");
-        Matcher matcher = pattern.matcher(targetChoice);
-        if (!matcher.find()) {
-            return false;
-        }
-
-        //今回はHigh and Low なので2択
-        // いっそ^[12]$でいいんじゃ...
-        int selectNumber = Integer.parseInt(targetChoice);
-        if (!(selectNumber == 1 || selectNumber == 2)){
-            return false;
-        }
-
-        return true;
+    enum Answer {
+        HIGH, LOW
     }
 
-    /**
-     * ユーザの選択肢の正誤を確認します
-     * @param first 基準値
-     * @param second 勝負値
-     * @param userAnswer ユーザが入力した答え(1:High 2:Low)
-     * @return int 判定結果(1:正解 2:はずれ 3:引き分け）
-     */
-    // 戻り値、enum使ってもよさそう
-    protected static int compareNumber(int first, int second, int userAnswer){
-        //Highが正解か？！Lowが正解か？！
-        int answer = 0;
-        if (first < second) {
-            answer = 1;
-        } else if (first > second){
-            answer = 2;
+    enum Result {
+        WIN, LOSE, DRAW
+    }
+
+    static class Game {
+
+        private final int firstNumber;
+
+        private final int secondNumber;
+
+        public Game(final int firstNumber, final int secondNumber) {
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
         }
 
-        // switch使ったほうが楽じゃない？
-        // ロジックとUIがごっちゃになってる＞＜
-        if (answer == 0) {
-            //1つ目の値と2つ目の値が等しい場合は勝負引き分け
-            System.out.println("引き分け！");
-            return 3;
-        } else if (answer == userAnswer){
-            System.out.println("あたり！ﾜ━ヽ(*´Д｀*)ﾉ━ｨ!!!");
-            return 1;
-        } else {
-            System.out.println("はずれ！(´・ω・｀)");
-            return 2;
+        public int getFirstNumber() {
+            return firstNumber;
+        }
+
+        public int getSecondNumber() {
+            return secondNumber;
+        }
+
+        public Result go(final Answer answer) {
+            final int got = Integer.compare(firstNumber, secondNumber);
+            if (got == 0) return Result.DRAW;
+            if (got < 0 && answer == Answer.HIGH) return Result.WIN;
+            if (got > 0 && answer == Answer.LOW) return Result.WIN;
+            return Result.LOSE;
         }
     }
 }
